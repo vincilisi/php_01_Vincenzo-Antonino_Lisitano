b08bf30335f0d44f1ad15d4a02e7630166e8558a
Vincenzo <lisitano95@gmail.com>
2024-10-06 15:17:06 +0200

    3ex

--- index.php
+++ index.php
@@ -4,18 +4,22 @@
 $int = 10;
 var_dump($int);
 
+define('INT' , 10);
 // float
 $float = 25.5;
 var_dump($float);
 
+define('FLOAT', 25.5);
 // string 
 
 $strng = 'Ciao cerco di capire';
 var_dump( $strng);
 
+define('STRING', 'Ciao cerco di capire' );
+
 // boolean
 $bool= false;
 var_dump($bool);
 
-
+define('BOOL',  false);
 ?>
