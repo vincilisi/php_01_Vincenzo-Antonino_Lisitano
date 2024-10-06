a5ecbd7cdde9b4028caf2c0bfffc03f2ed210deb
Vincenzo <lisitano95@gmail.com>
2024-10-06 09:45:28 +0200

    2ex

--- index.php
+++ index.php
@@ -2,16 +2,20 @@
 
 // integer
 $int = 10;
+var_dump($int);
 
 // float
 $float = 25.5;
+var_dump($float);
 
 // string 
 
 $strng = 'Ciao cerco di capire';
+var_dump( $strng);
 
 // boolean
 $bool= false;
+var_dump($bool);
 
 
 ?>
