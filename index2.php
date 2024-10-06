118254a24bed102bea82428afc8d43eb6700a59c
Vincenzo <lisitano95@gmail.com>
2024-10-06 15:40:31 +0200

    speriamo

---
+++ index2.php
@@ -0,0 +1,17 @@
+<?php
+$text =[
+$text1 = 'Marco',
+$text2= 'hai',
+$text_3= 'sete',
+$text4= '?',
+$text_5 = 'Perché',
+$te_xt6= 'hai',
+$text7 = 'bevuto',
+$text8='tutto',
+];
+
+foreach ($text as $testo) {
+    echo $testo . "\n";
+}
+
+?>
